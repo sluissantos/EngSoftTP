94e1207e4a6b8e80e533be3f7f8451c2a96bee26
sluissantos <luis.santos@logpyx.com>
2022-12-13 21:02:17 -0300

    mudanca das redes sociais

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <Typography component="footer" className={style.footer}>
             <span className={style.iconsContainer}>
-                <a href="https://www.instagram.com/davi_emediato/">
+                <a href="">
                     <img
                         src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
                         className={style.icon}
@@ -15,7 +15,7 @@ const Footer = () => {
                     />
                 </a>
                 <Divider />
-                <a href="https://twitter.com/Savio_Campolina">
+                <a href="">
                     <img
                         src="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1.png"
                         className={style.icon}
@@ -29,11 +29,11 @@ const Footer = () => {
             </>
             <span>
                 <Typography variant="span" marked="left" gutterBottom>
-                    made with (◍•ᴗ•◍)❤️ by savio, iury, lucca and leo
+                    made by Fernado, João, Luis e Rafael
                 </Typography>
                 <br />
                 <Typography variant="caption">
-                    "A dor é passageira, mas desistir é para sempre."
+                    "A dor é passageira (lá ele), mas desistir é para sempre."
                 </Typography>
             </span>
         </Typography>
